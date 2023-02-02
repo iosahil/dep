6db4989b1b04e5b84e994fbe2cee12010bc1f851
Sahil Choudhary <sahil844101@gmail.com>
2023-02-02 23:19:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         content: {
             /* Location of file where it is hosted */
             location: {
-                url: "your.pdf",
+                url: "/your.pdf",
                 /*
                 If the file URL requires some additional headers, then it can be passed as follows:-
                 headers: [
